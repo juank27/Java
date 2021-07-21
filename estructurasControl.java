d9965885549f727801d8955f5423f0eeb34a8b01
juank27 <juankruiz27@gmail.com>
2021-07-21 18:58:07 -0500

    operadores logicos

--- estructurasControl.java
+++ estructurasControl.java
@@ -20,4 +20,19 @@ public class estructurasControl {
    public static double valorAbsoluto2(double x) {
       return (x >= 0) ? x : -x;
    }
+      //operador logico
+   public static boolean condicional(boolean p, boolean q) {
+      if (p == true && q == false) {
+         return false;
+      } else {
+         return true;
+      }
+   }
+   public static boolean condicional2(boolean p, boolean q) {
+      if (p) {
+         return q;
+      } else {
+         return true;
+      }
+   }
 }
