113004bdae0aeebb549393fd6459cdb438ce7b10
juank27 <juankruiz27@gmail.com>
2021-07-21 17:48:03 -0500

    Manejo de Funciones

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hola mundo");
+      System.out.println(f(5.0));
+   }
+
+   public static double f(double x) 
+   {
+      return x * x;
+   }
+}
