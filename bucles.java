acdd38b17a7f9f1ac61b7e9bc94a03db7eb6f9e2
juank27 <juankruiz27@gmail.com>
2021-07-22 18:26:43 -0500

    Ciclo For

--- bucles.java
+++ bucles.java
@@ -2,6 +2,7 @@ public class bucles {
    public static void main(String[] args) {
       //Ciclo while
       cicloWhile();
+      suma(5);
    }
    //ciclo While
    public static void cicloWhile() {
@@ -10,5 +11,14 @@ public class bucles {
          System.out.println(i);
          i++;
       }
+   }
+   //ciclo for
+   public static int suma(int n) {
+      int s = 0;
+      for (int i = 0; i <= n; i++) {
+         System.out.println(i);
+         s = s+i;
+      }
+      return s;
    }
 }
