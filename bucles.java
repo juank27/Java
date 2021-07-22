b7f84603e16e7c877eaf143181969f371dfd7023
juank27 <juankruiz27@gmail.com>
2021-07-22 17:51:58 -0500

    Ciclo while

---
+++ bucles.java
@@ -0,0 +1,14 @@
+public class bucles {
+   public static void main(String[] args) {
+      //Ciclo while
+      cicloWhile();
+   }
+   //ciclo While
+   public static void cicloWhile() {
+      int i = 0;
+      while (i <= 6) {
+         System.out.println(i);
+         i++;
+      }
+   }
+}
