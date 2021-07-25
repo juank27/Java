88632eeedf58a5ac87ffc7d1b70a8f3f5f21a45d
juank27 <juankruiz27@gmail.com>
2021-07-24 21:04:35 -0500

    Arreglos operaciones basicas

--- ArreglosMatrices.java
+++ ArreglosMatrices.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+//A los arreglos tambien debe definirse un tipo de Dato
 public class ArreglosMatrices {
    public static void main(String[] args) {
-      
+      Scanner sc = new Scanner(System.in);
+      lee_arreglo_enteros(sc, 3);
+   }
+   //lee un arreglo
+   public static int[] lee_arreglo_enteros(Scanner sc, int n) {
+      int[] x = new int[n];
+      for (int i = 0; i < n; i++) {
+         System.out.println("Componente " + i + "-´esima?");
+         x[i] = sc.nextInt();
+      }
+      return x;
+   }
+   //posicion max de un arreglo
+   public static int pos_maximo(int[] A) {
+      int m = 0;
+      for (int i = 0; i < A.length; i++) {
+         if (A[i] > A[m])
+            m = i;
+      }
+      return m;
    }
 }
